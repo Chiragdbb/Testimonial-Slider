d8af7ba8ad048f4f5d4c91f940b20d427d36c98c
Chiragdbb <schirag414@gmail.com>
2023-08-13 21:19:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import { useState } from "react";
+import reviews from "./data";
+
+function App() {
+	const [userReviews, setUserReviews] = useState(reviews);
+
+	return (
+		<div>
+			<h1></h1>
+		</div>
+	);
+}
+
+export default App;
